8978431905d7a0f99170b513d1d41f66e259aa0f
Peter Kos <pkos91@icloud.com>
2018-12-21 23:29:44 -0500

    Added new dat, temp work

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,17 +8,22 @@
 		<!-- Let's show a table of RSOs -->
 		<div class="columns is-centered">
 			<div class="column is-half">
-				<table class="table is-hoverable is-fullwidth">
+				<table class="table is-hoverable is-fullwidth is-striped">
 					<thead>
 						<th class="has-text-centered">Logo</th>
 						<th class="has-text-centered">Name</th>
 						<th class="has-text-centered">Description</th>
 					</thead>
 					<tbody>
-						<tr v-for="rso in rsos">
-							<td> {{ getImage(rso) }} <img :src="imageURLs[rso.imageTag]" /></td>
-							<td>{{ rso.logo }}</td>
+						<tr v-for="rso in rsos" :key="rso.imageTag">
+							<td class="">
+								<!-- @FIXME: Image aspect ratioz -->
+								{{ getImage(rso) }}
+								<!-- <img :src="imageURLs[rso.imageTag]" class="image is-96x96"/> -->
+								<p>{{imageURLs[rso.imageTag]}}</p>
+							</td>
 							<td>{{ rso.name }}</td>
+							<td>{{ rso.description }}</td>
 						</tr>
 					</tbody>
 				</table>
@@ -63,6 +68,16 @@
 				var storageRef = Firebase.storage().ref()
 				var firstImageRef = storageRef.child(currentRSO.imageTag.toString() + ".png")
 
+
+				// if (firstImageRef == null) {
+				// 	console.log("not null")
+				// 	return
+				// } else {
+				// 	console.log("not null")
+				// }
+				// console.log(currentRSO.imageTag)
+
+				// @FIXME Fixme, please.
 				// Don't grab URL if url already exists
 				// Precondition: imageTag is a index corresponding to the RSO
 				if (this.imageURLs[currentRSO.imageTag]) {
@@ -71,9 +86,10 @@
 
 				// Actually grab it
 				firstImageRef.getDownloadURL().then(function(url) {
-						current.imageURLs.push(url)
+					current.imageURLs.push(url)
+					console.log(url)
 				}).catch(function(error) {
-					console.log(error)
+					// console.log(error)
 				})
 
 			},
