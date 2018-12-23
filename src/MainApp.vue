e25f57f237b08184ab7d5125353e8338ebe0b710
Peter Kos <pkos91@icloud.com>
2018-12-23 13:41:36 -0500

    testing feaatured rso category layout

--- src/MainApp.vue
+++ src/MainApp.vue
@@ -63,6 +63,124 @@
 			</div>
 
 		</section>
+
+		<!-- Section of Tech RSOs -->
+		<section class="section techSection is-medium">
+			<div class="columns is-vcentered">
+				<div class="column is-one-quarter">
+					<h1 class="title is-1 has-text-white">Tech</h1>
+				</div>
+				<div class="column">
+					<div class="tile is-ancestor">
+
+						<!-- "Column" 1 -->
+						<div class="tile is-parent is-vertical">
+							<div class="tile is-child notification is-dark">
+								<h1 class="title is-4">Dubstech</h1>
+							</div>
+						</div>
+
+						<!-- "Column" 2 -->
+						<div class="tile is-vertical">
+							<div class="tile is-parent is-vertical ">
+								<div class="tile is-child notification is-dark">
+									<h1 class="title is-4">Dubstech 1.1</h1>
+								</div>
+								<div class="tile is-child notification is-dark">
+									<h1 class="title is-4">Dubstech 1.2</h1>
+								</div>
+							</div>
+							<div class="tile is-parent">
+								<div class="tile is-vertical notification is-white">
+									<h1 class="title is-4">Dubstech 2</h1>
+									<h1 class="subtitle">Cool information or something</h1>
+								</div>
+							</div>
+						</div>
+
+						<!-- "Column" 3 -->
+						<div class="tile is-parent is-vertical">
+							<div class="tile is-child notification is-warning">
+								<h1 class="title is-4">Dubstech</h1>
+							</div>
+							<div class="tile is-child notification is-dark">
+								<h1 class="title is-4">Dubstech 2</h1>
+							</div>
+						</div>
+
+						<!-- "Column" 4 -->
+						<div class="tile is-parent is-vertical">
+							<div class="tile is-child notification is-warning">
+								<h1 class="title is-4">Dubstech</h1>
+							</div>
+							<div class="tile is-child notification is-info">
+								<h1 class="title is-4">Dubstech 2</h1>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- Section of Tech RSOs -->
+		<section class="section outreachSection is-medium">
+			<div class="columns is-vcentered">
+
+				<div class="column is-three-quarters">
+					<div class="tile is-ancestor">
+
+						<!-- "Column" 1 -->
+						<div class="tile is-parent is-vertical">
+							<div class="tile is-child notification is-white">
+								<h1 class="title is-4">Dubstech</h1>
+							</div>
+						</div>
+
+						<!-- "Column" 2 -->
+						<div class="tile is-vertical">
+							<div class="tile is-parent is-vertical ">
+								<div class="tile is-child notification is-white">
+									<h1 class="title is-4">Dubstech 1.1</h1>
+								</div>
+								<div class="tile is-child notification is-white">
+									<h1 class="title is-4">Dubstech 1.2</h1>
+								</div>
+							</div>
+							<div class="tile is-parent">
+								<div class="tile is-vertical notification is-white">
+									<h1 class="title is-4">Dubstech 2</h1>
+									<h1 class="subtitle">Cool information or something</h1>
+								</div>
+							</div>
+						</div>
+
+						<!-- "Column" 3 -->
+						<div class="tile is-parent is-vertical">
+							<div class="tile is-child notification is-warning">
+								<h1 class="title is-4">Dubstech</h1>
+							</div>
+							<div class="tile is-child notification is-white">
+								<h1 class="title is-4">Dubstech 2</h1>
+							</div>
+						</div>
+
+						<!-- "Column" 4 -->
+						<div class="tile is-parent is-vertical">
+							<div class="tile is-child notification is-warning">
+								<h1 class="title is-4">Dubstech</h1>
+							</div>
+							<div class="tile is-child notification is-info">
+								<h1 class="title is-4">Dubstech 2</h1>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="column">
+					<h1 class="title is-1 has-text-white has-text-right">Outreach</h1>
+				</div>
+			</div>
+		</section>
+
 		<hr>
 		<!-- All subviews go inside here! -->
 		<section class="section">
